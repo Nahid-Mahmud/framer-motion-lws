49c00e0c4b5b746af3f6402e5f78bc0a0ab28d3f
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-08-08 00:13:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Simple from "./components/Simple";
+
+function App() {
+  return (
+    <>
+      <Simple />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Simple.jsx
@@ -0,0 +1,13 @@
+import { motion } from "framer-motion";
+
+const Simple = () => {
+    return (
+        <motion.div 
+      animate= {{
+        
+      }}
+      className="bg-red-500 border w-32 h-32 rounded-full"></motion.div>
+    );
+};
+
+export default Simple;
