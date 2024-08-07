faa86274c2e46688e47140196c42509a3128c0da
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-08-08 00:18:33 +0600

    brief of initial and animate property

--- src/components/Simple.jsx
+++ src/components/Simple.jsx
@@ -1,13 +1,23 @@
 import { motion } from "framer-motion";
 
 const Simple = () => {
-    return (
-        <motion.div 
-      animate= {{
-        
+  return (
+    <motion.div
+      initial={{
+        scale: 0,
+        opacity: 0,
       }}
-      className="bg-red-500 border w-32 h-32 rounded-full"></motion.div>
-    );
+      animate={{
+        scale: 1,
+        opacity: 1,
+        transition: {
+          duration: 1,
+          delay: 0.5,
+        },
+      }}
+      className="bg-red-500 border w-32 h-32 rounded-full"
+    ></motion.div>
+  );
 };
 
 export default Simple;
