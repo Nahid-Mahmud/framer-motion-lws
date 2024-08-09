1f9f3e619e03dfcc39644ef1bb2317aeec936249
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-08-09 15:30:14 +0600

    images with stagger animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import ButtonTap from "./components/ButtonTap";
 import Counter from "./components/Counter";
+import Images from "./components/Images";
 import Keyframe from "./components/Keyframe";
 import ScrollRevel from "./components/ScrollRevel";
 import Simple from "./components/Simple";
+import Stagger from "./components/Stagger";
 import TextMotion from "./components/TextMotion";
 import Variants from "./components/Variants";
 import TransitionType from "./TransitionType";
@@ -20,7 +22,9 @@ function App() {
       {/* <TransitionType /> */}
       {/* <Variants /> */}
       {/* <Counter /> */}
-      <ScrollRevel />
+      {/* <ScrollRevel /> */}
+      {/* <Stagger /> */}
+      <Images />
     </>
   );
 }
---
+++ src/components/Images.jsx
@@ -0,0 +1,93 @@
+import Bags from "../assets/Bags.jpg";
+import Cloathing from "../assets/cloathings.jpg";
+import Computers from "../assets/Computers.jpg";
+import Electronics from "../assets/ellectronics.jpg";
+import FootWear from "../assets/Footwear.jpg";
+import Furniture from "../assets/Furniture.jpg";
+import Jewelary from "../assets/Jewelry.jpg";
+import { motion, useAnimate, stagger } from "framer-motion";
+import { useState, useEffect } from "react";
+
+const staggerImages = stagger(0.1, { startDelay: 0.15 });
+
+const ImagesArray = [
+  {
+    id: 1,
+    image: Bags,
+  },
+  {
+    id: 2,
+    image: Cloathing,
+  },
+  {
+    id: 3,
+    image: Computers,
+  },
+  {
+    id: 4,
+    image: Electronics,
+  },
+  {
+    id: 5,
+    image: FootWear,
+  },
+  {
+    id: 6,
+    image: Furniture,
+  },
+  {
+    id: 7,
+    image: Jewelary,
+  },
+];
+
+const Images = () => {
+  // state for show Hide images
+  const [showImages, setShowImages] = useState(false);
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    animate(
+      ".images",
+      {
+        opacity: showImages ? 1 : 0,
+      },
+      {
+        duration: 0.5,
+        delay: showImages ? staggerImages : 0,
+      }
+    );
+  }, [showImages, animate]);
+
+  return (
+    <div className="p-10" ref={scope}>
+      <motion.button
+        className="imageButton text-purple-900 mb-3 px-3 py-2  border-2 border-purple-400 bg-purple-300 rounded-md"
+        //   animation
+        whileTap={{ scale: 0.9 }}
+        whileHover={{
+          scale: 1.07,
+        }}
+        onClick={() => setShowImages(!showImages)}
+      >
+        {showImages ? "Hide Images" : "Show Images"}
+      </motion.button>
+
+      {/* Images */}
+
+      <div className="grid grid-cols-3 gap-4">
+        {ImagesArray.map((image) => (
+          <motion.div
+            key={image.id}
+            className="images rounded-md overflow-hidden h-80 w-auto"
+            whileHover={{ scale: 1.1 }}
+          >
+            <img src={image.image} alt="images" />
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Images;
